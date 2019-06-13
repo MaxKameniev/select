07f5026e20f228203a3c5a233104dcb423f8ac85
Max Kameniev <mkamen@softserveinc.com>
2019-06-13 13:16:18 +0300

    updated fetch

--- index.js
+++ index.js
@@ -4,39 +4,32 @@ const form = document.createElement('form');
 
 root.append(form);
 
-const config = {
-    country: ''
-}
+const config = {};
 
 const URL = "./db.json";
 
 const getData = () => fetch(URL)
     .then(response => response.json())
     .then(data => {
-        for (let creator of selects(data)) creator.next();
+        getSelection(data);
     })
     .catch(error => console.log(`Fetch error - ${error}`));
 
-function* selects(data) {
-    yield* createSelect('country', data);
-    yield* createSelect('cities', data);
-    yield* createSelect('streets', data);
-}
+const getSelection = (data) => {
+    createSelect('country', data);
+    createSelect('cities', data);
+    createSelect('streets', data);
+} 
 
 const selectChanged = (select, key) => {
-    const selected = select.options[select.selectedIndex].value;
-    config[key] = selected;
-    console.log(config[key]);
-    console.log(key);
-    debugger;
-    return selected;
+    config[key] = select.options[select.selectedIndex].value;
 }
 
 const defaultOption = (key, select) => {
     const option = document.createElement('option');
     select.append(option);
     option.textContent = `Choose the ${key}`;
-    option.setAttribute('value', key);
+    option.setAttribute('value', 'default');
 }
 
 const createOptions = (data, select, key) => {
@@ -53,7 +46,7 @@ const createSelect = (key, data) => {
     const select = document.createElement('select');
     form.append(select);
     select.setAttribute('name', key);
-    select.setAttribute('onchange', `selectChanged(${select}, ${key})`);
+    select.onclick = () => selectChanged(select, key);
     createOptions(data, select, key);
 }
 
